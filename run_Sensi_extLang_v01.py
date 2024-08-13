d6ba8c5fa8cf9868beb712c4730d4323757bf0b7
sebygaa <sebyga@gmail.com>
2024-08-14 00:25:44 +0900

    "Extended Langmuir model is tested."

---
+++ run_Sensi_extLang_v01.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+
+k_list = [ 0.01, 0.02, 0.05, 0.10, 0.20,]
+v_list = [ 0.01, 0.02, 0.05, 0.10, 0.20,]
+
+for kk in k_list:
+    for vv in v_list:
+        os.system('nohup python maonly_extLang_argv.py '+ str(vv)+ ' ' + str(kk) +
+                '> v' +str(vv)+'k' +str(kk) + '.log &') 
