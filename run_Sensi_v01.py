e47fd2ea6202a18e27628b1af97ac765b8715e80
sebygaa <sebyga@gmail.com>
2024-08-15 20:19:47 +0900

    Broader MTC and velocity values

--- run_Sensi_v01.py
+++ run_Sensi_v01.py
@@ -7,13 +7,8 @@ v_list = [0.001, 0.002, 0.005, 0.01]
 a = datetime.now()
 aa = a.strftime("%Y%m%d%H")
 
-base_path = os.getcwd()
-dirnam = 'run_IAST_or_'+ aa
-os.mkdir(dirnam)
-os.chdir(dirnam)
 
 for kk in k_list:
     for vv in v_list:
         os.system('nohup python maonly_ori_argv.py '+ str(vv)+ ' ' + str(kk) +
                 '> v' +str(vv)+'k' +str(kk) + '.log &') 
-os.chdir(base_path)
