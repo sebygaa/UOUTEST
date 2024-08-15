64cb01bc0eb6b47126c9c7030339072ea6d3331b
sebygaa <sebyga@gmail.com>
2024-08-15 11:44:11 +0900

    k v modification

--- run_Sensi_v01.py
+++ run_Sensi_v01.py
@@ -1,11 +1,19 @@
 import os
 import numpy as np
+from datetime import datetime
+k_list = [0.0005, 0.001, 0.002, 0.005, 0.01, 0.02]
+v_list = [0.001, 0.002, 0.005, 0.01]
 
-k_list = [ 0.01, 0.02, 0.05, 0.10, 0.20,]
-v_list = [ 0.01, 0.02, 0.05, 0.10, 0.20,]
+a = datetime.now()
+aa = a.strftime("%Y%m%d%H")
+
+base_path = os.getcwd()
+dirnam = 'run_IAST_or_'+ aa
+os.mkdir(dirnam)
+os.chdir(dirnam)
 
 for kk in k_list:
     for vv in v_list:
         os.system('nohup python maonly_ori_argv.py '+ str(vv)+ ' ' + str(kk) +
                 '> v' +str(vv)+'k' +str(kk) + '.log &') 
-
+os.chdir(base_path)
