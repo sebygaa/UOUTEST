0d0163a008bc0bc37e322920ce54ef53b92680e3
sebygaa <sebyga@gmail.com>
2024-08-17 18:44:40 +0900

    separate crop

--- run_Sensi_oriLang_v01.py
+++ run_Sensi_oriLang_v01.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from datetime import datetime
 from move2dir import move
+import time
 k_list = [1e-9, 1e-8, 1e-7, 1e-6, 1e-5]
 v_list = [0.001, 0.002, 0.005, 0.01]
 
@@ -13,4 +14,3 @@ for kk in k_list:
         os.system('nohup python maonly_ori_argv.py '+ str(vv)+ ' ' + str(kk) +
                 '> v' +str(vv)+'k' +str(kk) + '.log &') 
 
-move('res_oriIAST')
